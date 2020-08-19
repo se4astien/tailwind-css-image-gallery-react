ed7b223b4d6aecce8e0a575bedc31b58f5e8ca73
sebv <sebv@sv.home>
2020-08-19 17:37:32 +0200

    update project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=7873545-e07779c4cd1941bf2f090ca6e&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
   return (
-    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-      <img src='https://source.unsplash.com/random' alt='' className='w-full' />
-      <div className='px-6 py-4'>
-        <div className='font-bold text-purple-500 text-xl mb-2'>
-          Photo by John Doe
+    <div className='container mx-auto'>
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && (
+        <h1 className='text-6xl text-center mx-auto'>Images not found...</h1>
+      )}
+
+      {isLoading ? (
+        <h1 className='text-6xl text-center mx-auto'>Loading...</h1>
+      ) : (
+        <div className='grid grid-cols-3 gap-4'>
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
         </div>
-        <ul>
-          <li>
-            <strong>Views: </strong> 400
-          </li>
-          <li>
-            <strong>Download: </strong> 300
-          </li>
-          <li>
-            <strong>Likes: </strong> 500
-          </li>
-        </ul>
-      </div>
-      <div className='px-6 py-4'>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag1
-        </span>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag2
-        </span>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag3
-        </span>
-      </div>
+      )}
     </div>
   );
 }
